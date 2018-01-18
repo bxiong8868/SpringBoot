2eb1614c6222a8b64fbd99b2d634d5e78c24c3d0
bxiong8868 <bxiong1@yahoo.com>
2018-01-18 14:11:14 -0600

    Add files via upload

---
+++ TodoService.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+  private final TodoJpaRepository todoRepository;
+  private CounterService counter;
+
+  //public TodoService(TodoJpaRepository todoRepository) {
+  public TodoService(TodoJpaRepository todoRepository, CounterService counter) {
+	  this.todoRepository = todoRepository;
+	  this.counter = counter;
+  }
+
+  public Todo addTodo(Todo todo) {
+    //return todoRepository.save(todo);
+	  Todo added = todoRepository.save(todo);
+	      counter.increment("demo.todos.added");
+	      return added;
+  }
+
+  public List<Todo> findAll() {
+    return todoRepository.findAll();
+  }
+
+  public Optional<Todo> findById(Long id) {
+    return todoRepository.findById(id);
+  }
+
+  public void delete(Todo todo) {
+    todoRepository.delete(todo);
+  }
+}
